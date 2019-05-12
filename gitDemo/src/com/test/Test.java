00a7d255450008d758b3d382669e2f72c99a0437
chejin <371559514@qq.com>
2019-05-12 12:45:40 +0800

    第一次更新

--- gitDemo/src/com/test/Test.java
+++ gitDemo/src/com/test/Test.java
@@ -3,8 +3,10 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-		//
-		System.out.println("hello git");
+		//第一次提交
+		System.out.println("hello git1");
+		//第二次提交
+		System.out.println("hello git2");
 	}
 
 }
