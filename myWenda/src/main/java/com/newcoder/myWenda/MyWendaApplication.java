f8be6ba9c0e93927e1702f4a372e5b5453dc28fb
chejin <371559514@qq.com>
2020-02-25 10:13:00 +0800

    最后一个版本了，之后使用课程自带项目

--- myWenda/src/main/java/com/newcoder/myWenda/MyWendaApplication.java
+++ myWenda/src/main/java/com/newcoder/myWenda/MyWendaApplication.java
@@ -2,9 +2,6 @@ package com.newcoder.myWenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 public class MyWendaApplication {
